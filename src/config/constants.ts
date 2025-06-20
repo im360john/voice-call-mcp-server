a89c8d3c936cce27930a33f6f82d55b80fa80b64
im360john <john@treez.io>
2025-06-20 15:27:13 -0700

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,6 +8,6 @@ export const LOG_EVENT_TYPES = [
 
 export const DYNAMIC_API_SECRET = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 export const SHOW_TIMING_MATH = false;
-export const VOICE = 'ballard';
+export const VOICE = 'ballad';
 export const RECORD_CALLS = process.env.RECORD === 'true';
 export const GOODBYE_PHRASES = ['bye'];
