acf5cbe4ca9da3ea9ac4b322b1c900bd6c1dd331
im360john <john@treez.io>
2025-06-20 14:57:33 -0700

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -10,4 +10,4 @@ export const DYNAMIC_API_SECRET = Math.random().toString(36).substring(2, 15) + 
 export const SHOW_TIMING_MATH = false;
 export const VOICE = 'sage';
 export const RECORD_CALLS = process.env.RECORD === 'true';
-export const GOODBYE_PHRASES = ['bye', 'goodbye', 'have a nice day', 'see you', 'take care'];
+export const GOODBYE_PHRASES = ['bye','take care'];
